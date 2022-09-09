5b8b863d92440349a241d6cf00432f56c87f3248
hasibferdous <hasibferdous.ict@gmail.com>
2022-09-10 01:54:29 +0600

    localstorage problems

---
+++ localstorage.js
@@ -0,0 +1,28 @@
+//level-1
+//1. clear your local storage
+//2. set a local storage item. key: name, value: Sakib khan
+//3. set a storage item. key: age, value: 40(button)
+//4. Get the local storage item you have set(button)
+//5. remove the local storage key 'name' & key 'age'
+//6. set a object as local storage item. The object is {firstName: 'Abraham', lastName: 'Linkon'}
+
+
+//level-2
+//1. html file will have a p tag and a button.
+//2. the p tag will show "the count" which is initially 0. (inirially the value in local storage is 0)
+//3. every time, after clicking the button, the number will be increased by 1 and the result will be shown in the p tag.
+//4. with every click on the button, the result will be stored in the local storage also.
+//5. now the result is stored in local storage. so, if you reload the page or close the tab and open again, the result of 
+//"the count" is not 0. rather it will show the value from local storage without reset.
+
+//level-3
+//1. Create a form.
+//2. take the input
+//3. if you click on the 'send name' button, the input will be set on the local storage like the picture. Do the same for other send buttons.
+//4. if you click on the 'delete name' button, the name key and value will be cleared form on the local storage. Do the same for other delete buttons.
+//5. if you click on the 'Reset' button, the name key and value will be cleared form on the local storage. 
+
+//level-4
+//1. All task are same as previous.
+//2. one extra thing is added here. "send" button.
+//3. if you click on the 'send' button, all of the inputs(name, email,message) will be set on the local storage at a time.
---
+++ practice.js
@@ -0,0 +1,9 @@
+//1.Some middle console log will be logged after 3.5 sec 
+//2. Take a number from users, using prompt and add 250 to that number.
+//and then display that number using alert.
+//3. What is cookies, writing in english at least 5-7 lines.
+//4. Write at least 3 difference between local storage and session storage.
+//5. Differences between cookies, localstorage and session storage
+//6. What is event loop in js.
+//how synchronous and asynchronous code works in js.
+//7.Tell 4 things that are asynchronous in js.
